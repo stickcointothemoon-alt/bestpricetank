4f539191ad5fa6b1ef2cdbc1932f8dac94c9d9c0
stickcointothemoon-alt <stickcointothemoon@gmail.com>
2026-04-14 21:11:17 +0200

    Update get-pl-prices.js

--- netlify/functions/get-pl-prices.js
+++ netlify/functions/get-pl-prices.js
@@ -13,8 +13,8 @@ exports.handler = async () => ({
     updated: '11.04.2026',
     zgorzelec_i:  { e5: 6.12, diesel: 7.58, lpg: 3.89, adblue: 4.19 },
     zgorzelec_ii: { e5: 6.12, diesel: 7.58, lpg: 3.94, adblue: 4.19 },
-    hradek:       { e5: 39,99, diesel: 40,99, lpg: 19.80 },
+    hradek:       { e5: 39.99, diesel: 40.99, lpg: 19.80 },
     regional_pl:  { e5: 6.14, diesel: 7.60, lpg: 3.89 },
-    regional_cz:  { e5: 39,99, diesel: 40,99, lpg: 19.80 },
+    regional_cz:  { e5: 39.99, diesel: 40.99, lpg: 19.80 },
   }),
 });
